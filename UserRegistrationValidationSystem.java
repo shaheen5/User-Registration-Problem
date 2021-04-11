34863e055cd73d75b7003f32221617f7183dff45
shaheen5 <miyashaheen1995@gmail.com>
2021-04-11 18:58:33 +0530

    Added : Code To Display Welcome Message

---
+++ UserRegistrationValidationSystem.java
@@ -0,0 +1,7 @@
+package Java_Regex;
+
+public class UserRegistrationValidationSystem {
+	public static void main(String args[]) {
+		System.out.println("Welcome to User Registration Validation System!");
+	}
+}
